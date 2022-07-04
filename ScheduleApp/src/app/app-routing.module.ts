409c021b014e17ad4de58437c08cd0744f21681f
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-07-04 14:55:35 +0300

    Ft: Create routing for add student form

--- ScheduleApp/src/app/app-routing.module.ts
+++ ScheduleApp/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UpdateFormComponent } from './form/update-form/update-form.component';
 import { PostFormComponent } from './form/post-form/post-form.component';
+import { AddStudentFormComponent } from './form/add-student-form/add-student-form.component';
 
 const routes: Routes = [
   { path: 'update', component: UpdateFormComponent },
   { path: 'post', component: PostFormComponent },
+  { path: 'add', component: AddStudentFormComponent },
+
 
 ];
 
