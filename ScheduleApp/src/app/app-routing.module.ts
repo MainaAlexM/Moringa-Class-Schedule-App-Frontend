72145b0029dfa9b201e5a3ffbd4625bf4bcbd3f8
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-07-04 11:55:31 +0300

    Add routing for post & update forms

--- ScheduleApp/src/app/app-routing.module.ts
+++ ScheduleApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateFormComponent } from './form/update-form/update-form.component';
+import { PostFormComponent } from './form/post-form/post-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'update', component: UpdateFormComponent },
+  { path: 'post', component: PostFormComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
