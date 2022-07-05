c6e6305b25048c18b01d67277123b51eae7590a6
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-07-05 16:52:53 +0300

    Ft: Update search component

--- ScheduleApp/src/app/app-routing.module.ts
+++ ScheduleApp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { UpdateFormComponent } from './form/update-form/update-form.component';
 import { PostFormComponent } from './form/post-form/post-form.component';
 import { AddStudentFormComponent } from './form/add-student-form/add-student-form.component';
 import { ScheduleComponent } from './schedules/schedule/schedule.component';
+import { SearchComponent } from './search/search/search.component';
 
 const routes: Routes = [
   { path: 'update', component: UpdateFormComponent },
   { path: 'post', component: PostFormComponent },
   { path: 'add', component: AddStudentFormComponent },
-  { path: 'schedule', component: ScheduleComponent},
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'search', component: SearchComponent },
 
 
 ];
