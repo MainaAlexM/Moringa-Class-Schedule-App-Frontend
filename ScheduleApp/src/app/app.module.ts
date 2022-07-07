15263c25ea718f9e28306af104fdfc8a89a0b0ec
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-07-07 16:02:12 +0300

    ft:generated new services

--- ScheduleApp/src/app/app.module.ts
+++ ScheduleApp/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ScheduleService } from './schedule.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ScheduleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
